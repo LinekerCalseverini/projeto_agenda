a184d01a78e632fabe6cdfafb2e2ce7f4d64d76b
LinekerCalseverini <lineker.calseverini@gmail.com>
2023-09-20 22:44:43 -0300

    Filtrando valores com Q e OR para o campo de pesquisa

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 
@@ -6,11 +7,39 @@ from django.http import Http404
 def index(request):
     contacts = Contact.objects.all() \
         .filter(show=True) \
-        .order_by('-id')[10:30]
+        .order_by('-id')[:10]
 
     context = {
         'contacts': contacts,
         'site_title': 'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects.all() \
+        .filter(show=True) \
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+    ) \
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
     }
 
     return render(
