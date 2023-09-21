fb18e25f4755ac18701184a8c5893024cb8ed09d
LinekerCalseverini <lineker.calseverini@gmail.com>
2023-09-21 18:07:19 -0300

    Usando um padrão comum para URLs de CRUD

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,19 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
-from django.http import Http404
 
 
 def index(request):
     contacts = Contact.objects.all() \
         .filter(show=True) \
-        .order_by('-id')[:10]
+        .order_by('-id')
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'contacts': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -37,8 +40,12 @@ def search(request):
     ) \
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'contacts': page_obj,
         'site_title': 'Search - ',
         'search_value': search_value
     }
@@ -54,9 +61,6 @@ def contact(request, contact_id):
     # single_contact = Contact.objects.filter(id=contact_id).first()
     single_contact = get_object_or_404(Contact, id=contact_id, show=True)
     contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
-
-    if single_contact is None:
-        raise Http404()
 
     context = {
         'contact': single_contact,
