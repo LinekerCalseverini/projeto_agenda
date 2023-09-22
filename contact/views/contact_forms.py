2c34bfcaa1d9c585fddadf83379259ccfe795478
LinekerCalseverini <lineker.calseverini@gmail.com>
2023-09-22 18:38:17 -0300

    Usando request.method e request.POST para saber dados da request

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 
 def create(request):
+    if request.method == 'POST':
+        request.POST.get('first_name')
+        request.POST.get('last_name')
+
     context = {}
 
     return render(
